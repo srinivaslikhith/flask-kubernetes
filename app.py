97df2e0e91fba4d325e29ffd74f308f5ea6f99ae
srinivaslikhith <likhithsrinivas9@gmail.com>
2025-06-27 15:20:13 -0700

    complete setup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host = 'localhost', port = 6379)
+r = redis.Redis(host = 'redis', port = 6379)
 
 @app.route('/')
 def hello():
